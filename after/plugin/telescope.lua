81440c06bf716b3a62dafd09eab0d1e378e03fec
alright <eml.alexey@gmail.com>
2023-07-31 22:22:16 -0700

    add live grep and buffers mappings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,6 +2,5 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>pf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
---vim.keymap.set('n', '<leader>ps', function()
---	builtin.grep_string({ search = vim.fn.input("Grep > ") });
---end)
+vim.keymap.set('n', '<leader><leader>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>pg', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
